85da363916e8b872f4e9131749e4c8657adf14d5
AMAN <aman.25161024@kiet.edu>
2026-02-17 10:20:43 +0530

    check user is eligible for vote or not using lambda expression

--- lambdaExpression/MainFunctionalInterface.java
+++ lambdaExpression/MainFunctionalInterface.java
@@ -1,13 +1,20 @@
 package lambdaExpression;
+@FunctionalInterface
 interface Calc{
-    void calculate(int n1, int n2);
+    //void calculate(int n1, int n2);
+    //int square(int n1);
+    //String square(int n1);
+    void calculate(int n1);
 }
 public class MainFunctionalInterface {
     public static void main(String[] args){
-        Calc a=(int n1,int n2)->{
-            //int num1=10,num2=30;
-            System.out.println("Sum is : "+(n1+n2));
+        Calc a = n1 -> {
+            if(n1>=18){
+                System.out.println("You can vote");
+            }else{
+                System.out.println("You cannot vote");
+            }
         };
-        a.calculate(10,30);
+        a.calculate(30);
     }
 }
