bedb986f746e23622a836d339848641fbae9034a
AMAN <aman.25161024@kiet.edu>
2026-03-16 12:55:31 +0530

    used stream API filtering

--- StreamUse.java
+++ StreamUse.java
@@ -4,6 +4,10 @@ public class StreamUse {
         int data[] = new int[]{23,45,67};
         List<Integer> list = Arrays.stream(data).boxed().toList();
         System.out.println(list);
+        //for each method + lambda
         list.forEach((value)->System.out.println(value));
+        //using method referencing
+        list.forEach(System.out::println);
+
     }
 }
